5cff049730752e0092163681b09fe652e11ba8d6
mikeian <zhouwenxiang@ciwei.net>
2024-11-28 17:01:04 +0800

    test commit

--- src/main/java/com/ian/mike/validator/HelloObjValidator.java
+++ src/main/java/com/ian/mike/validator/HelloObjValidator.java
@@ -7,8 +7,12 @@ package com.ian.mike.validator;
  **/
 public class HelloObjValidator {
 
+    private static ThreadLocal<String> flag = new ThreadLocal<>();
+
     public static void hello(String msg) {
         System.out.println("validator hello msg : " + msg);
+        flag.set("test");
+        flag.get();
     }
 
     public static void main(String[] args) {
